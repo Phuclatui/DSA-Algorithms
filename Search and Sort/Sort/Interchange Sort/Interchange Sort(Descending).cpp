f2f76c04c58c15d5af09562b98934b09482253b6
Phuclatui <mydreamand202@gmail.com>
2026-04-04 09:21:37 +0700

    Update

---
+++ Search and Sort/Sort/Interchange Sort/Interchange Sort(Descending).cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<ctime>
+#define MAX 1000
+using namespace std;
+
+void addArray(int array[])
+{
+	for (int i = 0; i < MAX; i++)
+	{
+		array[i] = rand() % 1000;
+	}
+}
+
+void showArray(const int array[])
+{
+	for (int i = 0; i < MAX; i++)
+	{
+		cout << array[i] << " ";
+	}
+	cout << endl;
+}
+
+void InterchangeSort(int array[]) // Descending
+{
+	for (int i = 0; i < MAX - 1; i++)
+	{
+		for (int j = i + 1; j < MAX; j++)
+		{
+			if (array[i] < array[j])
+				swap(array[i], array[j]);
+		}
+	}
+}
+
+
+
+int main()
+{
+	srand(time(0));
+	int array[MAX];
+
+	cout << "Original array: ";
+	addArray(array);
+	showArray(array);
+
+
+	cout << "\nAfter Sort: ";
+	InterchangeSort(array);
+	showArray(array);
+}
---
+++ Search and Sort/Sort/Interchange Sort/README.md
@@ -0,0 +1,16 @@
+### Interchange Sort-C++
+
+### DATA STRUCTURE
+
+* Algorithm: Interchange Sort
+
+### COMPLEXITY ANALYSIS
+
+Best case: O(1)
+
+* The array is sorted
+
+Worst case: O(n^2)
+
+
+
