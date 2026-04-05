d0109090954237f54b9cc6bb74b61fff27aa0a3a
Phuclatui <mydreamand202@gmail.com>
2026-04-05 10:10:06 +0700

    Update

--- Binay Search Tree/Red-Black Tree/Red-Black tree.cpp
+++ Binay Search Tree/Red-Black Tree/Red-Black tree.cpp
@@ -1,12 +1,12 @@
-#include<iostream>
+﻿#include<iostream>
 using namespace std;
 
-enum Color {RED, BLACK};
+enum Color { RED, BLACK };
 
 struct Node
 {
 	int key;
-	Node* left, * right;
+	Node* right, * left;
 	Node* parent;
 	Color color;
 };
@@ -16,17 +16,7 @@ void init(Node*& root)
 	root = nullptr;
 }
 
-Node* createNode(const int& x)
-{
-	Node* p = new Node;
-	p->key = x;
-	p->left = p->right = p->parent = nullptr;
-	p->color = RED;
-
-	return p;
-}
-
-void LeftRotate(Node*& root, Node* p)
+void leftRotate(Node*& root, Node* p)
 {
 	Node* q = p->right;
 	p->right = q->left;
@@ -38,7 +28,7 @@ void LeftRotate(Node*& root, Node* p)
 
 	if (p->parent == nullptr)
 		root = q;
-	else if (p == p->parent->left)
+	else if (p->parent->left == p)
 		p->parent->left = q;
 	else
 		p->parent->right = q;
@@ -47,7 +37,7 @@ void LeftRotate(Node*& root, Node* p)
 	q->left = p;
 }
 
-void RightRotate(Node*& root, Node* p)
+void rightRotate(Node*& root, Node* p)
 {
 	Node* q = p->left;
 	p->left = q->right;
@@ -56,6 +46,7 @@ void RightRotate(Node*& root, Node* p)
 		q->right->parent = p;
 
 	q->parent = p->parent;
+
 	if (p->parent == nullptr)
 		root = q;
 	else if (p->parent->left == p)
@@ -67,7 +58,17 @@ void RightRotate(Node*& root, Node* p)
 	q->right = p;
 }
 
-void fixing_Insert(Node*& root, Node* p)
+Node* createNode(const int& x)
+{
+	Node* p = new Node;
+	p->key = x;
+	p->left = p->right = p->parent = nullptr;
+
+	p->color = RED;
+	return p;
+}
+
+void insert_fix(Node*& root, Node* p)
 {
 	while (p != root && p->parent->color == RED)
 	{
@@ -87,16 +88,15 @@ void fixing_Insert(Node*& root, Node* p)
 			{
 				if (p == parent->right)
 				{
-					LeftRotate(root, parent);
+					leftRotate(root, parent);
 					p = parent;
 					parent = p->parent;
 				}
 
-				RightRotate(root, grandparent);
+				rightRotate(root, grandparent);
 				swap(parent->color, grandparent->color);
 				p = parent;
 			}
-
 		}
 		else
 		{
@@ -111,12 +111,12 @@ void fixing_Insert(Node*& root, Node* p)
 			{
 				if (p == parent->left)
 				{
-					RightRotate(root, parent);
+					rightRotate(root, parent);
 					p = parent;
 					parent = p->parent;
 				}
 
-				LeftRotate(root, grandparent);
+				leftRotate(root, grandparent);
 				swap(parent->color, grandparent->color);
 				p = parent;
 			}
@@ -125,6 +125,42 @@ void fixing_Insert(Node*& root, Node* p)
 	root->color = BLACK;
 }
 
+void insert(Node*& root, const int& x)
+{
+	Node* q = createNode(x);
+	Node* p = root;
+	Node* parent = nullptr;
+
+	while (p != nullptr)
+	{
+		parent = p;
+		if (p->key > x)
+			p = p->left;
+		else
+			p = p->right;
+	}
+
+	q->parent = parent;
+	if (parent == nullptr)
+		root = q;
+	else if (parent->key > x)
+		parent->left = q;
+	else
+		parent->right = q;
+
+	insert_fix(root, q);
+}
+
+
+void lnr(Node* root)
+{
+	if (root != nullptr)
+	{
+		lnr(root->left);
+		cout << root->key << " ";
+		lnr(root->right);
+	}
+}
 
 void fixing_delete(Node*& root, Node* p)
 {
@@ -133,14 +169,17 @@ void fixing_delete(Node*& root, Node* p)
 		if (p == p->parent->left)
 		{
 			Node* sibling = p->parent->right;
+
+			//Case 1: mục đích là chuyển sibling thành black, case sau dựa vào con mới của sibling
 			if (sibling != nullptr && sibling->color == RED)
 			{
 				sibling->color = BLACK;
 				p->parent->color = RED;
-				LeftRotate(root, p->parent);
+				leftRotate(root, p->parent);
 				sibling = p->parent->right;
-			}
 
+			}
+			//Case 2: p double black, và sibling có 2 con là black (tính cả NIL)
 			if ((sibling->left == nullptr || sibling->left->color == BLACK) && (sibling->right == nullptr || sibling->right->color == BLACK))
 			{
 				sibling->color = RED;
@@ -148,68 +187,134 @@ void fixing_delete(Node*& root, Node* p)
 			}
 			else
 			{
+				//case 3: con gần là RED, con xa là BLack/NIL
 				if (sibling->right == nullptr || sibling->right->color == BLACK)
 				{
 					if (sibling->left != nullptr)
 					{
 						sibling->left->color = BLACK;
 						sibling->color = RED;
-						RightRotate(root, sibling);
-						sibling = p->parent->right;
+						rightRotate(root, sibling);
+						sibling = p->parent->right; // sibling có con phải là RED
 					}
 				}
 
-					sibling->color = p->parent->color;
-					p->parent->color = BLACK;
-					if (sibling->right != nullptr)
-					{
-						sibling->right->color = BLACK;
-					}
-					LeftRotate(root, p->parent);
-					p = root;
-				}
+				sibling->color = p->parent->color; // kế thừa màu color của parent nhằm duy trì black height
+				p->parent->color = BLACK; // dùng parent (nếu nó RED) làm nguồn BLACK để triệt tiêu double black ở nhánh p
+				if (sibling->right != nullptr)
+					sibling->right->color = BLACK; // bù black nếu như sibling = parent color - red
+				leftRotate(root, p->parent); //dùng con xa RED của sibling làm nguồn BLACK để bù cho nhánh còn lại và tránh RED–RED
+				p = root;
+
+			}
 		}
-		else
-		{
+
+		else {
 			Node* sibling = p->parent->left;
 			if (sibling != nullptr && sibling->color == RED)
 			{
 				sibling->color = BLACK;
 				p->parent->color = RED;
-				RightRotate(root, p->parent);
+				rightRotate(root, p->parent);
 				sibling = p->parent->left;
 			}
-
-			if ((sibling->left == nullptr || sibling->left->color == BLACK) && (sibling->right == nullptr || sibling->right->color == BLACK))
-			{
+			if ((sibling->left == nullptr || sibling->left->color == BLACK) && (sibling->right == nullptr || sibling->right->color == BLACK)) {
 				sibling->color = RED;
 				p = p->parent;
 			}
-			else
-			{
-				if (sibling->left == nullptr || sibling->left->color == BLACK)
+			else {
+				if (sibling->left == nullptr
+					|| sibling->left->color == BLACK)
 				{
 					if (sibling->right != nullptr)
-					{
-						sibling->left->color = BLACK;
-						sibling->color = RED;
-						LeftRotate(root, sibling);
-						sibling = p->parent->left;
-					}
+						sibling->right->color = BLACK;
+					sibling->color = RED;
+					leftRotate(root, sibling);
+					sibling = p->parent->left;
 				}
-
 				sibling->color = p->parent->color;
 				p->parent->color = BLACK;
-
 				if (sibling->left != nullptr)
-				{
 					sibling->left->color = BLACK;
-				}
-				RightRotate(root, p->parent);
+				rightRotate(root, p->parent);
 				p = root;
 			}
 		}
-		p->color = BLACK;
+	}
+	p->color = BLACK;
+}
+
+
+void deleteNode(Node*& root, const int& x)
+{
+	Node* p = root;
+	Node* parent = nullptr;
+
+	Color deletedColor;
+	Node* fixNode = nullptr;
+
+	while (p != nullptr && p->key != x)
+	{
+		parent = p;
+		if (p->key > x)
+			p = p->left;
+		else
+			p = p->right;
+	}
+
+	if (p == nullptr)
+	{
+		cout << "The node is not found!!!" << endl;
+		return;
+	}
+
+	if (p->left != nullptr && p->right != nullptr)
+	{
+		Node* temp = p->right;
+		parent = p;
+		while (temp->left != nullptr)
+		{
+			parent = temp;
+			temp = temp->left;
+		}
+		p->key = temp->key;
+		p = temp;
+	}
+	deletedColor = p->color;
+
+	Node* r;
+	if (p->left == nullptr)
+		r = p->right;
+	else
+		r = p->left;
+
+	fixNode = r;
+
+	if (parent == nullptr)
+	{
+		root = r;
+		if (r != nullptr)
+			r->parent = nullptr;
+	}
+	else
+	{
+		if (parent->left == p)
+			parent->left = r;
+		else
+			parent->right = r;
+
+		if (r != nullptr)
+			r->parent = parent;
+	}
+
+	delete p;
+
+	if (deletedColor == BLACK)
+	{
+		if (fixNode != nullptr)
+			fixing_delete(root, fixNode);
+		else
+			fixing_delete(root, parent); // double black NIL
 	}
 }
 
@@ -220,5 +325,14 @@ int main()
 	Node* root;
 	init(root);
 
+	insert(root, 10);
+	insert(root, 30);
+	insert(root, 20);
+
+	lnr(root);
+	cout << endl;
+
+	deleteNode(root, 20);
+	lnr(root);
 
 }
